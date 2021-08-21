7252a97d6c4e46883a2c86e53eda1d6fe13f24f3
A-miin <zulaykaisaeva@gmail.com>
2021-08-21 17:06:29 +0600

    Add pagination and search to profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,27 +1,122 @@
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.db.models import Q
-
-# Create your views here.
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from django.views.generic import DetailView, UpdateView
+from django.views.generic.list import MultipleObjectMixin, ListView
 
 from accounts.models import User
 from accounts.form import UserUpdateForm
 
-class UserView(DetailView):
-    model = User
+
+from django.views.generic import (
+    DetailView,
+    UpdateView,
+
+)
+
+from django.db.models import Q
+from django.utils.http import urlencode
+from announcements.form import SearchForm
+
+# Create your views here.
+from announcements.models import Announcement
+
+class UserView(ListView):
+
     template_name = 'profile.html'
-    context_object_name = 'user_obj'
+    model = Announcement
+    context_object_name = 'announcements'
+    ordering = ('-created_at')
+    paginate_by = 9
+    paginate_orphans = 0
+
+
+    def get(self, request, **kwargs):
+        self.form = SearchForm(request.GET)
+        self.search_data = self.get_search_data()
+
+        return super(UserView, self).get(request, **kwargs)
+
+    def get_queryset(self):
+        user = self.get_object()
+        queryset = super().get_queryset()
+        if self.request.user == user:
+            queryset = queryset.filter(Q(is_active=True) & Q(author=user))
+        else:
+            queryset = queryset.filter(Q(author=user) & Q(status='Publicated') & Q(is_active=True))
+
+
+        if self.search_data:
+            queryset = queryset.filter(
+                Q(title__icontains=self.search_data) |
+                Q(text__icontains=self.search_data)
+            )
+        return queryset
+
+    def get_search_data(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search_value']
+        return None
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        user = self.get_object()
-        if self.request.user==user:
-            context['announcements'] = user.announcements.filter(is_active=True)
-        else:
-            context['announcements'] = user.announcements.filter(Q(status='Publicated') & Q(is_active=True))
+
+        context['user_obj']=self.get_object()
+
+
+        context['search_form'] = self.form
+
+
+        if self.search_data:
+            context['query'] = urlencode({'search_value': self.search_data})
 
         return context
+
+    def get_object(self):
+        user = get_object_or_404(User, id=self.kwargs.get('pk'))
+        print(user)
+        return user
+
+# class UserView(ListView):
+#     model = User
+#     template_name = 'profile.html'
+#     context_object_name = 'announcements'
+#     paginate_by = 9
+#     paginate_orphans = 0
+#
+#     def get(self, request, **kwargs):
+#         self.form = SearchForm(request.GET)
+#         self.search_data = self.get_search_data()
+#         return super(UserView, self).get(request, **kwargs)
+#
+#
+#     def get_search_data(self):
+#         if self.form.is_valid():
+#             return self.form.cleaned_data['search_value']
+#         return None
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         user = self.get_object()
+#         queryset = Announcement.objects.all()
+#         if self.search_data:
+#             queryset = Announcement.objects.filter(
+#                 Q(title__icontains=self.search_data) |
+#                 Q(text__icontains=self.search_data)
+#             )
+#         if self.request.user==user:
+#             context['announcements'] = queryset.filter(Q(is_active=True) & Q(author=user))
+#         else:
+#             context['announcements'] = queryset.filter(Q(author=user) & Q(status='Publicated') & Q(is_active=True))
+#
+#         context['search_form'] = self.form
+#
+#         if self.search_data:
+#             context['query'] = urlencode({'search_value': self.search_data})
+#
+#         return context
+#
+
+
+
 
 class UserUpdateView(UpdateView):
     model = User

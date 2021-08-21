1112a752dcfdb8fbb49e4915f246a6a8b05be488
A-miin <zulaykaisaeva@gmail.com>
2021-08-21 16:29:03 +0600

    Add phone number validation

--- accounts/form.py
+++ accounts/form.py
@@ -1,7 +1,9 @@
 from django import forms
 from accounts.models import User
-
+from phonenumber_field.formfields import PhoneNumberField
 class UserUpdateForm(forms.ModelForm):
+    phone_number = PhoneNumberField(
+        error_messages={"invalid": "Формат телефона только в виде +996 XXX XXX XXX"})
     class Meta:
         model = User
         fields = ('phone_number',)

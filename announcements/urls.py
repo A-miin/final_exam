074f17ad1ce88d16dab449d8bfe659778674f1be
A-miin <zulaykaisaeva@gmail.com>
2021-08-21 13:16:00 +0600

    Add new announcements list for staff

--- announcements/urls.py
+++ announcements/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from announcements.views import (IndexView,
                                 AnnouncementView,
                                 CreateAnnouncementView,
+ReviewAnnouncementView,
                                  )
 
 app_name = 'annoncement'
@@ -11,5 +12,5 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('<int:pk>/', AnnouncementView.as_view(), name='announcement-detail'),
     path('add/', CreateAnnouncementView.as_view(), name='announcement-create'),
-
+    path('moderator/', ReviewAnnouncementView.as_view(), name='announcement-review'),
 ]
--- announcements/views.py
+++ announcements/views.py
@@ -18,7 +18,7 @@ from announcements.form import SearchForm, AnnouncementForm
 from announcements.models import Announcement
 
 # Create your views here.
-class IndexView(LoginRequiredMixin, ListView):
+class IndexView(ListView):
 
     template_name = 'index.html'
     model = Announcement
@@ -75,3 +75,43 @@ class CreateAnnouncementView(LoginRequiredMixin, CreateView):
         announcement.save()
 
         return redirect('annoncement:index')
+
+class ReviewAnnouncementView(ListView):
+
+    template_name = 'review.html'
+    model = Announcement
+    context_object_name = 'announcements'
+    ordering = ('-created_at')
+    paginate_by = 9
+    paginate_orphans = 0
+
+
+    def get(self, request, **kwargs):
+        self.form = SearchForm(request.GET)
+        self.search_data = self.get_search_data()
+        return super(ReviewAnnouncementView, self).get(request, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(status='For moderation')
+
+        if self.search_data:
+            queryset = queryset.filter(
+                Q(title__icontains=self.search_data) |
+                Q(text__icontains=self.search_data)
+            )
+        return queryset
+
+    def get_search_data(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search_value']
+        return None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_form'] = self.form
+
+        if self.search_data:
+            context['query'] = urlencode({'search_value': self.search_data})
+
+        return context
